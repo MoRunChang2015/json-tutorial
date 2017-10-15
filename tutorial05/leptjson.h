5c939a066b9af45bfcf130b8b3d7ed6bdedc2510
Mr.Ac <709403987ac@gmail.com>
2017-10-15 16:50:18 +0800

    finish tutorial05

--- tutorial05/leptjson.h
+++ tutorial05/leptjson.h
@@ -3,34 +3,51 @@
 
 #include <stddef.h> /* size_t */
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRING,
+  LEPT_ARRAY,
+  LEPT_OBJECT
+} lept_type;
 
 typedef struct lept_value lept_value;
 
 struct lept_value {
-    union {
-        struct { lept_value* e; size_t size; }a;    /* array:  elements, element count */
-        struct { char* s; size_t len; }s;           /* string: null-terminated string, string length */
-        double n;                                   /* number */
-    }u;
-    lept_type type;
+  union {
+    struct {
+      lept_value* e;
+      size_t size;
+    } a; /* array:  elements, element count */
+    struct {
+      char* s;
+      size_t len;
+    } s;      /* string: null-terminated string, string length */
+    double n; /* number */
+  } u;
+  lept_type type;
 };
 
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR,
-    LEPT_PARSE_NUMBER_TOO_BIG,
-    LEPT_PARSE_MISS_QUOTATION_MARK,
-    LEPT_PARSE_INVALID_STRING_ESCAPE,
-    LEPT_PARSE_INVALID_STRING_CHAR,
-    LEPT_PARSE_INVALID_UNICODE_HEX,
-    LEPT_PARSE_INVALID_UNICODE_SURROGATE,
-    LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,
+  LEPT_PARSE_INVALID_VALUE,
+  LEPT_PARSE_ROOT_NOT_SINGULAR,
+  LEPT_PARSE_NUMBER_TOO_BIG,
+  LEPT_PARSE_MISS_QUOTATION_MARK,
+  LEPT_PARSE_INVALID_STRING_ESCAPE,
+  LEPT_PARSE_INVALID_STRING_CHAR,
+  LEPT_PARSE_INVALID_UNICODE_HEX,
+  LEPT_PARSE_INVALID_UNICODE_SURROGATE,
+  LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET
 };
 
-#define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
+#define lept_init(v)       \
+  do {                     \
+    (v)->type = LEPT_NULL; \
+  } while (0)
 
 int lept_parse(lept_value* v, const char* json);
 
